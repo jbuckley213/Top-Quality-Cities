6fda06634687b53f5c47510e3434ef36c0632df6
Joseph Buckley <jbuckley213@gmail.com>
2020-11-01 18:09:48 +0100

    validator.js

--- scripts/Validator.js
+++ scripts/Validator.js
@@ -1,1 +1,52 @@
 "use strict";
+
+class Validator {
+  constructor() {
+    this.invalidEmailError = "Enter a vaild email address";
+    this.emailExistsError = "The entered email address us already taken";
+    this.passwordError = "Password must be at least 6 characters long";
+    this.repeatPasswordError = "Password and repeat password must match";
+
+    this.errors = {
+      invalidEmailError: this.invalidEmailError,
+      passwordError: this.passwordError,
+      repeatPasswordError: this.repeatPasswordError,
+      emailExistsError: this.emailExistsError,
+    };
+  }
+
+  validateEmail = (email) => {
+    if (this.emailSyntaxIsValid(email)) {
+      delete this.errors.invalidEmailError;
+    } else {
+      this.errors.invalidEmailError = this.invalidEmailError;
+    }
+  };
+
+  validatePassword = (password) => {
+    validateRepeatPassword = (password, repeatPassword) => {};
+  };
+
+  emailSyntaxIsValid = (email) => {
+    const emailRegEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+
+    const emailIsValid = emailRegEx.test(email);
+    return emailIsValid;
+  };
+
+  validateUniqueEmail = (email) => {
+    const users = db.getAllUsers();
+
+    let emailUnique = true;
+
+    users.forEach((userObj) => {
+      if (userObj.email === email) {
+        emailUnique = false;
+      }
+    });
+  };
+
+  getErrors = () => {};
+}
+
+const validator = new Validator();
--- scripts/index.js
+++ scripts/index.js
@@ -7,43 +7,43 @@ const barcaImg = document.querySelector("#barca-img");
 const dublinImg = document.querySelector("#dublin-img");
 const vancouverImg = document.querySelector("#vancouver-img");
 
-function debounce(func, wait = 20, immediate = true) {
-  var timeout;
-  return function () {
-    var context = this,
-      args = arguments;
-    var later = function () {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    };
-    var callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
-  };
-}
+// function debounce(func, wait = 20, immediate = true) {
+//   var timeout;
+//   return function () {
+//     var context = this,
+//       args = arguments;
+//     var later = function () {
+//       timeout = null;
+//       if (!immediate) func.apply(context, args);
+//     };
+//     var callNow = immediate && !timeout;
+//     clearTimeout(timeout);
+//     timeout = setTimeout(later, wait);
+//     if (callNow) func.apply(context, args);
+//   };
+// }
 
-window.addEventListener("scroll", debounce(scroll));
+// window.addEventListener("scroll", debounce(scroll));
 
-function scroll(e) {
-  console.log(window.pageYOffset);
-  if (window.innerWidth > 1040) {
-    if (pageYOffset > 575 && pageYOffset < 750) {
-      //650
-      barcaDiv.classList.add("stick");
-      dublinDiv.classList.add("inactive-img");
-      vancouverDiv.classList.add("inactive-img");
-      barcaDiv.classList.add("test-barca");
-    } else if (pageYOffset < 750) {
-      barcaDiv.classList.remove("stick");
-      barcaDiv.classList = "test-barca";
-      //barcaImg.classList.add("img-change");
-    } else if (pageYOffset > 950 && pageYOffset < 1200) {
-      barcaDiv.classList.add("test-dublin");
-      barcaDiv.classList.remove("test-barca");
-    } else if (pageYOffset > 1200) {
-      barcaDiv.classList.add("test-vancouver");
-      barcaDiv.classList.remove("test-dublin");
-    }
-  }
-}
+// function scroll(e) {
+//   console.log(window.pageYOffset);
+//   if (window.innerWidth > 1040) {
+//     if (pageYOffset > 575 && pageYOffset < 750) {
+//       //650
+//       barcaDiv.classList.add("stick");
+//       dublinDiv.classList.add("inactive-img");
+//       vancouverDiv.classList.add("inactive-img");
+//       barcaDiv.classList.add("test-barca");
+//     } else if (pageYOffset < 750) {
+//       barcaDiv.classList.remove("stick");
+//       barcaDiv.classList = "test-barca";
+//       //barcaImg.classList.add("img-change");
+//     } else if (pageYOffset > 950 && pageYOffset < 1200) {
+//       barcaDiv.classList.add("test-dublin");
+//       barcaDiv.classList.remove("test-barca");
+//     } else if (pageYOffset > 1200) {
+//       barcaDiv.classList.add("test-vancouver");
+//       barcaDiv.classList.remove("test-dublin");
+//     }
+//   }
+// }
